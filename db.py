0509cf42624258c66f2ec704285fb1aba2ce326d
Nurkahfi Amran Rahmada <nurkahfirahmada@gmail.com>
2025-08-05 19:01:02 +0700

    add: base atlas setup

--- db.py
+++ db.py
@@ -1,7 +1,19 @@
-from pymongo import AsyncMongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-client = AsyncMongoClient("localhost", 27017)
-db = client["sobat-warung-dummy"]
+URI = f"mongodb+srv://{os.getenv("DB_USERNAME")}:{os.getenv("DB_PASSWORD")}@xplore.1v68hbs.mongodb.net/?retryWrites=true&w=majority&appName=xplore"
+
+client = MongoClient(URI, server_api=ServerApi("1"))
+db = client[os.getenv("DB_NAME")]
 
 owner = db["owner"]
 warung = db["warung"]
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
