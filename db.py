d4c9b45f3364ba7d0f142ba9f2f5896eb397b276
ichsan-frds <bsmbangirfan@gmail.com>
2025-08-06 15:11:59 +0700

    fix: penyesuaian db.py

--- db.py
+++ db.py
@@ -1,19 +1,13 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from pymongo import AsyncMongoClient, ServerApi
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 URI = f"mongodb+srv://{os.getenv("DB_USERNAME")}:{os.getenv("DB_PASSWORD")}@xplore.1v68hbs.mongodb.net/?retryWrites=true&w=majority&appName=xplore"
 
-client = MongoClient(URI, server_api=ServerApi("1"))
+client = AsyncMongoClient(URI, server_api=ServerApi("1"))
 db = client[os.getenv("DB_NAME")]
 
 owner = db["owner"]
 warung = db["warung"]
-
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
