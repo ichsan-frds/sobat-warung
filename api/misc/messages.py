19caf2d9bb7ad96b8d80fb25e780f1fbd79a0025
ichsan-frds <bsmbangirfan@gmail.com>
2025-08-10 20:54:40 +0700

    refactor: register

--- api/misc/messages.py
+++ api/misc/messages.py
@@ -4,27 +4,66 @@ Yuk mulai dengan isi namamu dulu!
 Ketik dengan format: *Nama : Nama Panggilan*  
 Contoh: *Nama : Budi*"""
 
+    EXCEPTION_WELCOME_MSG = """Format salah!
+Ketik dengan format: *Nama : Nama Panggilan*  
+Contoh: *Nama : Budi*"""
+
     def REG_WARUNG_MSG(owner_name: str):
         return f"""👋 Hai *{owner_name}*!
 Sekarang lanjut dengan isi nama warungmu.                    
 Ketik dengan format: *Warung : Nama Warung*  
 Contoh: *Warung : Ramirez Jatinangor*"""
 
-    EXCEPTION_WELCOME_MSG = """Format salah!
-Ketik dengan format: *Nama : Nama Panggilan*  
-Contoh: *Nama : Budi*"""
+    EXCEPTION_REG_WARUNG_MSG = """Format salah!
+Ketik dengan format: *Warung : Nama Warung*  
+Contoh: *Warung : Ramirez Jatinangor*"""
 
-    def REG_LOCATION_MSG(warung_name: str):
+    def REG_WILAYAH_MSG(warung_name: str):
         return f"""Terimakasih *{warung_name}*!
-Terakhir silahkan isi lokasi warungmu.
+Kemudian silahkan isi wilayah warungmu.  
+Contoh: *Desa : Bojong Kulur*
+*Kecamatan : Gunung Putri*
+*Kabupaten : Bogor*
+*Provinsi : Jawa Barat*
+
+❗Tulis setiap informasi di baris baru (tekan *Enter*
+atau *Shift+Enter* di PC/Laptop)"""
+    
+    EXCEPTION_REG_WILAYAH_MSG = """Format salah!
+❗Tulis setiap informasi di baris baru (tekan *Enter*
+atau *Shift+Enter* di PC/Laptop)
+Contoh: *Desa : Bojong Kulur*
+*Kecamatan : Gunung Putri*
+*Kabupaten : Bogor*
+*Provinsi : Jawa Barat*"""
+
+    def REG_LOCATION_MSG():
+        return """Kemudian silahkan isi lokasi warungmu.
 Kamu bisa kirim *share location* langsung (jika posisi sekarang di warung),
 atau *isi manual* koordinatnya.
 Ketik dengan format: *Latitude : angka, Longitude : angka*  
 Contoh: *Latitude : -6.23, Longitude : 106.82*"""
 
-    EXCEPTION_REG_WARUNG_MSG = """Format salah!
-Ketik dengan format: *Warung : Nama Warung*  
-Contoh: *Warung : Ramirez Jatinangor*"""
+    EXCEPTION_REG_LOCATION_MSG = """Format salah!
+Kamu bisa kirim *share location* langsung (jika posisi sekarang di warung),
+atau *isi manual* koordinatnya.
+Ketik dengan format: *Latitude : angka, Longitude : angka*  
+Contoh: *Latitude : -6.23, Longitude : 106.82*"""
+
+    def REG_TIPE_MSG():
+        return """Kemudian silahkan isi tipe warungmu.
+Balas *A/B/C/D* sesuai *jumlah barang merk berbeda* yang dijual warung-mu:
+A. Menjual > 100 Barang Beda Merk
+B. Menjual 50 - 100 Barang Beda Merk
+C. Menjual 20 - 50 Barang Beda Merk
+D. Menjual < 20 Barang Beda Merk"""
+    
+    EXCEPTION_REG_TIPE_MSG = """Format salah!
+Balas *A/B/C/D* sesuai *jumlah barang merk berbeda* yang dijual warung-mu:
+A. Menjual > 100 Barang Beda Merk
+B. Menjual 50 - 100 Barang Beda Merk
+C. Menjual 20 - 50 Barang Beda Merk
+D. Menjual < 20 Barang Beda Merk"""
 
     def MENU_MSG(owner_name: str):
         return f"""👋 Hai {owner_name}!
@@ -33,9 +72,8 @@ Balas *angka* untuk pilih menu berikut :
 1. Setor Penjualan Hari Ini
 2. Prediksi Permintaan Besok
 3. Cek Stok Warung"""
-
-    EXCEPTION_REG_LOCATION_MSG = """Format salah!
-Kamu bisa kirim *share location* langsung (jika posisi sekarang di warung),
-atau *isi manual* koordinatnya.
-Ketik dengan format: *Latitude : angka, Longitude : angka*  
-Contoh: *Latitude : -6.23, Longitude : 106.82*"""
+    
+    def MENU_1_MSG():
+        return """Apa saja yang terjual hari ini?  
+Ketik dengan format: *Terjual : Barang, jumlah; Barang, jumlah; ...*  
+Contoh: *Terjual : Indomie, 10; Teh Gelas, 5*"""
--- api/routes/whatsapp.py
+++ api/routes/whatsapp.py
@@ -3,7 +3,7 @@ from api.core.twilio import client
 import os
 from api.misc.messages import Messages
 from api.misc.states import State
-from api.core.database import owner, warung
+from api.core.database import owner, warung, stock
 
 router = APIRouter()
 
@@ -17,18 +17,18 @@ def send_message(to: str, body: str):
 @router.post("/")
 async def whatsapp_webhook(request: Request):
     form_data = await request.form()
-    print("Incomming:\n", form_data)
+    print("Incoming:\n", form_data) # UNCOMMENT DI PRODUCTION
     
     state = (await owner.find_one({"phone_number": form_data["From"]}) or {}).get("state")
     
     if state is None:
         await owner.insert_one({
             "phone_number": form_data["From"],
-            "state": State.WAITING_FOR_NAME.value
+            "state": State.INPUT_NAMA.value
         })
-        
         send_message(form_data["From"], Messages.WELCOME_MSG)
-    elif state == State.WAITING_FOR_NAME.value:
+    
+    elif state == State.INPUT_NAMA.value:
         try:
             if "Nama :" in form_data["Body"]:
                 owner_name = form_data["Body"].split("Nama :")[1].strip()
@@ -49,6 +49,7 @@ async def whatsapp_webhook(request: Request):
             send_message(form_data["From"], Messages.REG_WARUNG_MSG(owner_name))
         except Exception:
             send_message(form_data["From"], Messages.EXCEPTION_WELCOME_MSG)
+    
     elif state == State.INPUT_NAMA_WARUNG.value:
         try:
             if "Warung :" in form_data["Body"]:
@@ -73,9 +74,47 @@ async def whatsapp_webhook(request: Request):
                 "owner_id": owner_id,
             })
 
-            send_message(form_data["From"], Messages.REG_LOCATION_MSG(warung_name))
+            send_message(form_data["From"], Messages.REG_WILAYAH_MSG(warung_name))
         except Exception:
             send_message(form_data["From"], Messages.EXCEPTION_REG_WARUNG_MSG)
+    
+    elif state == State.INPUT_WILAYAH_WARUNG.value:
+        try:
+            if (
+                ("Desa :" in form_data["Body"] or "Kelurahan :" in form_data["Body"]) 
+                and "Kecamatan :" in form_data["Body"] 
+                and ("Kota :" in form_data["Body"] or "Kabupaten :" in form_data["Body"]) 
+                and "Provinsi :" in form_data["Body"]
+            ):
+                lines = form_data["Body"].split("\n")
+                desa_kel = kecamatan = kota_kab = provinsi = None
+                for line in lines:
+                    if line.startswith("Desa :") or line.startswith("Kelurahan :"):
+                        desa_kel = line.split(":", 1)[1].strip()
+                    elif line.startswith("Kecamatan :"):
+                        kecamatan = line.split(":", 1)[1].strip()
+                    elif line.startswith("Kota :") or line.startswith("Kabupaten :"):
+                        kota_kab = line.split(":", 1)[1].strip()
+                    elif line.startswith("Provinsi :"):
+                        provinsi = line.split(":", 1)[1].strip()
+                if not desa_kel or not kecamatan or not kota_kab or not provinsi:
+                    raise HTTPException(
+                        status_code=400, detail="Tidak boleh ada yang kosong")
+                
+            owner_data = await owner.find_one({"phone_number": form_data["From"]})
+            owner_id = owner_data.get("_id")
+            await owner.update_one({"phone_number": form_data["From"]}, {"$set": {"state": "INPUT_LOKASI_WARUNG"}})
+
+            await warung.update_one({"owner_id": owner_id},
+                                    {"$set": {"desa/kelurahan": desa_kel, 
+                                            "kecamatan": kecamatan,
+                                            "kota/kabupaten": kota_kab,
+                                            "provinsi": provinsi}})
+                
+            send_message(form_data["From"], Messages.REG_LOCATION_MSG())
+        except Exception:
+            send_message(form_data["From"], Messages.EXCEPTION_REG_WILAYAH_MSG)
+
     elif state == State.INPUT_LOKASI_WARUNG.value:  
         try:
             if form_data["MessageType"] == "location":
@@ -108,21 +147,31 @@ async def whatsapp_webhook(request: Request):
                     }
                 })
 
-            send_message(form_data["From"], Messages.MENU_MSG(owner_name))
+            send_message(form_data["From"], Messages.REG_TIPE_MSG())
         except Exception:
             send_message(form_data["From"], Messages.EXCEPTION_REG_LOCATION_MSG)
-    elif state == "MENU":
+    
+    elif state == State.TIPE_WARUNG.value:
+        try:
+            if form_data["Body"] in ['A', 'B', 'C', 'D']:
+                owner_data = await owner.find_one({"phone_number": form_data["From"]})
+                owner_id = owner_data.get("_id")
+                owner_name = owner_data.get("owner_name", "Sobat Warung")
+                await owner.update_one({"phone_number": form_data["From"]}, {"$set": {"state": "MENU"}})
+
+                await warung.update_one({"owner_id": owner_id},{"$set": {"type": form_data["Body"]}})
+                
+            else:
+                raise HTTPException(
+                    status_code=400, detail="Format tidak sesuai")
+            send_message(form_data["From"], Messages.MENU_MSG(owner_name))
+        except Exception:
+            send_message(form_data["From"], Messages.EXCEPTION_REG_TIPE_MSG)
+
+    elif state == State.MENU.value:
         if form_data["Body"] == '1': 
-            # UNCOMMENT KALAU INGIN MENGIRIM PESAN DARI BOT KE USER ($0.01 / msg)
-            message = client.messages.create(
-                    to = form_data["From"],
-                    from_ = os.getenv("FROM_WA_NUMBER"),
-                    body=
-                    """Apa saja yang terjual hari ini?  
-Ketik dengan format: *Terjual : Barang, jumlah; Barang, jumlah; ...*  
-Contoh: *Terjual : Indomie, 10; Teh Gelas, 5*"""
-)
-        elif "Setor :" in form_data["Body"]:
+            send_message(form_data["From"], Messages.MENU_1_MSG())
+        elif "Terjual :" in form_data["Body"]:
             pass
         
         elif form_data["Body"] == '2':
@@ -131,6 +180,11 @@ Contoh: *Terjual : Indomie, 10; Teh Gelas, 5*"""
         
         elif form_data["Body"] == '3':
             # IF TOKO SUDAH INPUT STOK, FETCH STOK DARI DATABASE
+            owner_data = await owner.find_one({"phone_number": form_data["From"]})
+            owner_id = owner_data.get("_id")
+            warung_data = await warung.find_one({"owner_id": owner_id})
+            warung_id = warung_data.get("_id")
+            stock_data = (await stock.find({"warung_id": warung_id}) or {}).get()
             pass
 
             # ELSE, SURUH TOKO INPUT STOK TERLEBIH DAHULU
