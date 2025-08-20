8c67b2b587dc934d9d9a2e0d611d610af992e787
ichsan-frds <bsmbangirfan@gmail.com>
2025-08-20 23:29:49 +0700

    fix: main

--- api/main.py
+++ api/main.py
@@ -4,8 +4,6 @@ from api.routes import api_router
 
 app = FastAPI(title="Sobat Warung")
 
-app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
-
 app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/sanity")
